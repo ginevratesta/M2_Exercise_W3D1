0c6d682efd94130a26022ca91ed355ace4ca637e
Ginevra Testa <ginevra99testaa@gmail.com>
2023-10-25 19:36:36 +0200

    last commit

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ Scrivi una funzione chiamata "boundary", che accetta un numero intero come param
 e ritorna true se tale parametro è incluso tra 20 e 100 (incluso) o se è esattamente uguale a 400.\n`);
 
 function boundary(z) {
-  if (z > 20 && z <= 100 || z ===  400) {
+  if ((z > 20 && z <= 100) || z === 400) {
     return true;
   } else {
     return false;
@@ -48,10 +48,10 @@ console.log(`\nESERCIZIO 3
 Scrivi una funzione chiamata "reverseString", che accetta una stringa come parametro e la ritorna invertita (es.: EPICODE => EDOCIPE).\n`);
 
 function reverseString(reverse) {
-    let array = reverse.split("");
-    let arrayReverse = [];
-  for(let i = array.length -1; i >= 0; i --){
-    arrayReverse.push(array[i])
+  let array = reverse.split("");
+  let arrayReverse = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    arrayReverse.push(array[i]);
   }
   return arrayReverse.join("");
 }
@@ -68,21 +68,24 @@ console.log(`\nESERCIZIO 4.
 Scrivi una funzione chiamata "upperFirst", che accetta una stringa come parametro e la ritorna rendendo maiuscola ogni lettera iniziale di ogni parola.\n`);
 
 function upperFirstWord(sentence) {
-    let words = sentence.split(" "); // Dividiamo la stringa in un array di parole
-  
-    for (let i = 0; i < words.length; i++) {
-      let word = words[i];
-      words[i] = word.charAt(0).toUpperCase() + word.slice(1); /* con .charAt(0) selezioniamo la prima lettera di ogni parola e con .toUpperCase() 
+  let words = sentence.split(" "); // Dividiamo la stringa in un array di parole
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    words[i] =
+      word.charAt(0).toUpperCase() +
+      word.slice(
+        1
+      ); /* con .charAt(0) selezioniamo la prima lettera di ogni parola e con .toUpperCase() 
       la rendiamo maiuscola, .slice(1) raggruppa ed esclude dal processo tutte le altre lettere di ogni parola a partire dalla seconda */
-    }
-  
-    return words.join(" "); // Rimettiamo di nuovo insieme le parole in una stringa
   }
-  
-  let finalSentence = upperFirstWord(`una splendida giornata di sole`);
-  console.log(`Questo è il risultato del quarto esercizio: ${finalSentence}`);
-  
-  
+
+  return words.join(" "); // Rimettiamo di nuovo insieme le parole in una stringa
+}
+
+let finalSentence = upperFirstWord(`una splendida giornata di sole`);
+console.log(`Questo è il risultato del quarto esercizio: ${finalSentence}`);
+
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n e ritorna un array contenente n numeri random contenuti tra 0 e 10.
 */
@@ -91,13 +94,13 @@ console.log(`\nESERCIZIO 5.
 Scrivi una funzione chiamata "giveMeRandom", che accetta come parametro un numero chiamato n 
 e ritorna un array contenente n numeri random contenuti tra 0 e 10.\n`);
 
-function giveMeRandom(n){
-    let randomNumbers = [];
-    for (let i = 0; i < n; i++) {
-      let randomNumber = Math.floor(Math.random() * 11); // math.random genera un numero casuale tra 0 e 10 e math.floor fa si che escano solo numeri interi
-      randomNumbers.push(randomNumber);
-    }
-    return randomNumbers;
+function giveMeRandom(n) {
+  let randomNumbers = [];
+  for (let i = 0; i < n; i++) {
+    let randomNumber = Math.floor(Math.random() * 11); // math.random genera un numero casuale tra 0 e 10 e math.floor fa si che escano solo numeri interi
+    randomNumbers.push(randomNumber);
+  }
+  return randomNumbers;
 }
 let randomResults = giveMeRandom(4);
 console.log(randomResults);
@@ -145,18 +148,17 @@ Scrivi una funzione chiamata "codify" che accetta una stringa come parametro.
 La funzione deve aggiungere la parola "code" all'inizio della stringa fornita e ritornare il risultato, 
 ma se la stringa fornita comincia proprio con "code" allora deve ritornarla senza modifiche.\n`);
 
-function codify(s){
-   if(s.indexOf(`code`) === 0){ //per questo esercizio usiamo .indexOf() che resistuisce l'indice di un elemento dentro un array o una stringa
-    return s
-   }    
-   else{
-    return `code ` + s
-   }
+function codify(s) {
+  if (s.indexOf(`code`) === 0) {
+    //per questo esercizio usiamo .indexOf() che resistuisce l'indice di un elemento dentro un array o una stringa
+    return s;
+  } else {
+    return `code ` + s;
+  }
 }
 
-
 let string = codify(`questa è una stringa`);
-console.log(`Questo è il risultato del terzo esercizio: ${string}`)
+console.log(`Questo è il risultato del terzo esercizio: ${string}`);
 
 /* ESERCIZIO 4
  Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
@@ -167,28 +169,31 @@ console.log(`Questo è il risultato del terzo esercizio: ${string}`)
 console.log(`\nESERCIZIO 4.
 Scrivi una funzione chiamata "check3and7" la quale accetta un numero intero positivo come parametro.
 La funzione deve controllare che tale parametro sia un multiplo di 3 o di 7, e in tal caso tornare true; altrimenti deve tornare false.
-SUGGERIMENTO: operatore modulo\n`)
- function check3and7(n){
-    if(n % 3 === 0 || n % 7 === 0){
-        return true
-    }
-    else{
-        return false
-    }
- }
+SUGGERIMENTO: operatore modulo\n`);
+function check3and7(n) {
+  if (n % 3 === 0 || n % 7 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
- let positiveNumber = check3and7(70);
- console.log(`Questo è il risultato del quarto esercizio: ${positiveNumber}`);
+let positiveNumber = check3and7(70);
+console.log(`Questo è il risultato del quarto esercizio: ${positiveNumber}`);
 
 /* ESERCIZIO 5
  Scrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.
 */
 /* SCRIVI QUI LA TUA RISPOSTA */
-console.log(`\nScrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.\n`)
+console.log(
+  `\nScrivi una funzione chiamata "cutString", che accetta una stringa come parametro e la ritorna senza il primo e l'ultimo carattere.\n`
+);
 
-function cutString(string){
-    return string.slice(1, -1) //Eliminiamo il primo e l'ultimo carattere dalla stringa con .slice()
+function cutString(string) {
+  return string.slice(1, -1); //Eliminiamo il primo e l'ultimo carattere dalla stringa con .slice()
 }
 
- let newPhrase = cutString(`ulteriore stringa a scopo dimostrativo per esercitarsi`);
- console.log(`Questo è il risultato del quinto esercizio: ${newPhrase}`);
+let newPhrase = cutString(
+  `ulteriore stringa a scopo dimostrativo per esercitarsi`
+);
+console.log(`Questo è il risultato del quinto esercizio: ${newPhrase}`);
